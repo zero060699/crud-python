0228a5f008ca372326412c424748c8585a9d5164
zero060699 <builoc191999@gmail.com>
2021-09-27 10:50:32 +0700

    news

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.home),
-    path('products', views.products),
+    path('products/', views.products),
     path('customer/', views.customer),
 ]
