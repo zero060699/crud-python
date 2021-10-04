de6dd729409b187b80f4d728a613ba76039329ca
zero060699 <builoc191999@gmail.com>
2021-10-04 16:28:08 +0700

    signal

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -38,9 +38,6 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name='customer')
-            user.group.add(group)
-            Customer.objects.create(user = user)
             messages.success(request, 'Account created for ' + username)
             return redirect('login')
     context = {'form': form,}
